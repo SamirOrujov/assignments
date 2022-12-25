17f949a79d78728f5a992505010b115d75169695
Samir Orujov <111826200+SamirOrujov@users.noreply.github.com>
2022-12-25 20:33:38 +0400

    C# practice task Model2 25.12.2022

---
+++ Assignments/ICalculate2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_practice_task.Models.Models2
+{
+
+    internal interface ITest
+    {
+
+    }
+    internal interface ICalculate
+    {
+        public void Sum();
+
+        public void Divide();
+
+        public void Multiply();
+    }
+
+    class Calculate : Animal, ICalculate, ITest
+    {
+        public void Divide()
+        {
+            Console.WriteLine(10/2);
+        }
+
+        public void Multiply()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Sound()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Sum()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assignments/Program2.cs
@@ -0,0 +1,53 @@
+﻿Test test = new();
+
+class Test
+{
+    public Test()
+    {
+        test(5);
+    }
+    void test()
+    {
+
+    }
+
+    void test(int num)
+    {
+
+    }
+}
+
+interface ICalculate
+{
+    void Sum();
+    virtual void Sum2()
+    {
+
+    }
+}
+
+class B : ICalculate
+{
+    public void Sum()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Sum2()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+class A : ICalculate
+{
+    public void Sum()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Sum2()
+    {
+        throw new NotImplementedException();
+    }
+}
