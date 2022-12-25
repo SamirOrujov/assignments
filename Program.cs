5649339c4c2abb01a6220e50f9fe6355f0a26294
Samir Orujov <111826200+SamirOrujov@users.noreply.github.com>
2022-12-25 20:26:26 +0400

    C# practice Model1

--- Program.cs
+++ Program.cs
@@ -1,44 +1,9 @@
-﻿using Calculator;
-using System.Net.Http.Headers;
+﻿using C_practice_task.Models1.Models2;
 
-Calculate generalInput = new();
+Student student = new();
 
-Console.Write("1ci ededi daxil ediniz: ");
+Teacher teacher = new();
 
-generalInput.input1 = int.Parse(Console.ReadLine());
+teacher.Sum();
 
-Console.Write("Emeliyyat isaresini daxil ediniz: ");
-
-generalInput.input2 = (Console.ReadLine());
-
-bool check = string.IsNullOrWhiteSpace(generalInput.input2); 
-
-Console.Write("2ci ededi daxil ediniz: ");
-
-generalInput.input3 = int.Parse(Console.ReadLine());
-
-if (generalInput.input2 == "+")
-{
-    var sum = generalInput.Sum();
-    Console.WriteLine($"Daxil etdiyiniz ededlerin cemi: {sum}");
-}
-else if (generalInput.input2 == "-")
-{
-    var extract = generalInput.Extract();
-    Console.WriteLine($"Daxil etdiyiniz ededlerin ferqi: {extract}");
-}
-else if (generalInput.input2 == "*")
-{
-    var multiply = generalInput.Multiply();
-    Console.WriteLine($"Daxil etdiyiniz ededlerin hasili: {multiply}");
-}
-else if (generalInput.input2 == "/")
-{
-    var divide = generalInput.Divide();
-    Console.WriteLine($"Daxil etdiyiniz ededlerin nisbeti: {divide}");
-}
-else
-{
-    Console.WriteLine("Dogru emeliyyat isaresi daxil ediniz");
-}
-
+student.Sum();
---
+++ Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_practice_task.Models1.Models2
+{
+    internal class Student : Person
+    {
+        public override void AMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
