eca770be14676b4c2258e2a405bc1ba5d6a98174
Samir Orujov <111826200+SamirOrujov@users.noreply.github.com>
2023-01-02 16:15:49 +0400

    v2

---
+++ Assignments/Custom reverse.cs
@@ -0,0 +1,16 @@
+﻿string sentence = Console.ReadLine();
+
+string[] words = sentence.Split(' ');
+
+string newsentence = string.Empty;
+
+for (int i = 0; i < words.Length; i++)
+{
+	for (int j = words[i].Length-1; j >= 0; j--)
+	{
+		newsentence += words[i][j];
+	}
+	newsentence += ' ';
+}
+
+Console.Write(newsentence);
